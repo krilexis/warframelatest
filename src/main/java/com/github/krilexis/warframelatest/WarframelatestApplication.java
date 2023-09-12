7c3fa7356f79f19ef273fd41b36613432adc0c77
krilexis <flying.fish@gmx.fr>
2023-09-12 20:10:16 +0200

    Init Spring project

---
+++ src/main/java/com/github/krilexis/warframelatest/WarframelatestApplication.java
@@ -0,0 +1,13 @@
+package com.github.krilexis.warframelatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarframelatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarframelatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/krilexis/warframelatest/WarframelatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.krilexis.warframelatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarframelatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
